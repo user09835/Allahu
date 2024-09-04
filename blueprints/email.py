463775814b30e3b64a8e2fb1191fa21cc375d5f9
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-09-04 21:57:15 +0600

    Update email.py

--- blueprints/email.py
+++ blueprints/email.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 email_blueprint = Blueprint('email', __name__, template_folder='../templates')
 
-@gen_blueprint.route('/email')
+@email_blueprint.route('/email')
 def email():
     return render_template('email.html')
