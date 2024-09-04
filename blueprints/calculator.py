3c0b2f37c12d77111f0d29ab75e79a158f4e0ed4
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-09-04 21:43:36 +0600

    Update calculator.py

--- blueprints/calculator.py
+++ blueprints/calculator.py
@@ -1,1 +1,7 @@
+from flask import Blueprint, render_template
 
+calculator_blueprint = Blueprint('calculator', __name__, template_folder='../templates')
+
+@calculator_blueprint.route('/age')
+def page1():
+    return render_template('calculator.html')
