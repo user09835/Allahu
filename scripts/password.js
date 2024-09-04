1e855ed7aa7dbab16d8b2c9a46da25e9b0058fe3
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-09-04 21:34:54 +0600

    Create password.js

---
+++ scripts/password.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById('password').value = password;
+}
