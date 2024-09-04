8d01a08215f70d571264d1bcc11e5f42105bce27
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-09-04 21:23:17 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import importlib
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+# Automatically register all blueprints from the 'blueprints' folder
+def register_blueprints(app):
+    blueprints_dir = os.path.join(os.path.dirname(__file__), 'blueprints')
+
+    for filename in os.listdir(blueprints_dir):
+        if filename.endswith('.py') and filename != '__init__.py':
+            module_name = f"blueprints.{filename[:-3]}"
+            module = importlib.import_module(module_name)
+            blueprint = getattr(module, f"{filename[:-3]}_blueprint")
+            app.register_blueprint(blueprint)
+
+register_blueprints(app)
+
+# Serve JavaScript files from 'scripts' folder
+@app.route('/scripts/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('scripts', filename)
+
+# Custom 404 Error Handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
